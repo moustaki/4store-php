3831390269f678a15215c778044714b7fe3ce6ce
Daniel O'Connor <daniel.oconnor@gmail.com>
2012-03-11 02:20:05 +1030

    Fix use of split as per 145c751890aa442c582ff1911ad6dddb56a84810

--- FourStore/Store.php
+++ FourStore/Store.php
@@ -89,7 +89,7 @@ class FourStore_Store {
     }
 
     public function add($graph, $turtle) {
-        $post_endpoint = array_shift(split("/sparql/", $this->_endpoint)) . "/data/";
+        $post_endpoint = array_shift(explode("/sparql/", $this->_endpoint)) . "/data/";
         $client = $this->client;
         $client->setUri($post_endpoint);
         $client->setParameterPost('graph', $graph);
